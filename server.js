1f758cbc01d69b7a43a0475ad28ac6107b8131bc
Jon <jonathanjackson424@gmail.com>
2019-12-06 16:47:30 -0500

    deploy commit

--- server.js
+++ server.js
@@ -10,13 +10,14 @@ const app = express();
 app.use(cors());
 app.use(helmet());
 
-app.use(morgan('dev'));
+const morganSetting = process.env.NODE_ENV === 'production' ? 'tiny' : 'common';
+app.use(morgan(morganSetting));
 
 app.use(function validateBearerToken(req, res, next) {
   const apiToken = process.env.API_TOKEN;
   const authToken = req.get('Authorization');
 
-  console.log('validate bearer token middleware');
+ 
   // move to the next middleware
   if (!authToken || authToken.split(' ')[1] !== apiToken) {
     return res.status(401).json({ error: 'Unauthorized request' });
@@ -63,9 +64,17 @@ function sortByAvgVote(res, avg_vote) {
   res.json(filteredList);
 }
 
+app.use((error, req, res, next) => {
+  let response;
+  if (process.env.NODE_ENV === 'production') {
+    response = { error: { message: 'server error' }};
+  } else {
+    response = { error };
+  }
+  res.status(500).json(response);
+});
 
-
-const PORT = 8080;
+const PORT = process.env.PORT || 8000;
 
 app.listen(PORT, () => {
   console.log(`Server listening at http://localhost:${PORT}`);
